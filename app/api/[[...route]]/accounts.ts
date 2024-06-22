7bf8816d12f816c5519724a564f19db16d1e8d3f
Nikhil chopra <nikhil0080012005@gmail.com>
2024-06-23 00:46:46 +0530

    just for fun

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -32,6 +32,44 @@ const app = new Hono()
                 .where(eq(accounts.userId , auth.userId))
             return c.json({ data });
     })
+    .get(
+      "/:id",
+      zValidator("param" , z.object({
+        id: z.string().optional(),
+      })),
+      clerkMiddleware(),
+      async(c) => {
+        const auth = getAuth(c);
+        const { id } = c.req.valid("param");
+
+        if(!id){
+          return c.json({ error : " Missing ID "} , 400);
+        }
+
+        if(!auth?.userId){
+          return c.json({ error : " unauthorised "} , 401);
+        }
+
+        const [data] = await db
+          .select({
+            id : accounts.id,
+            name : accounts.name,
+          })
+          .from(accounts)
+          .where(
+            and(
+              eq(accounts.userId , auth.userId),
+              eq(accounts.id , id),
+            ),
+          );
+
+          if(!data){
+            return c.json({error : "Not Found"} , 404);
+          }
+
+          return c.json({data});
+      }
+    )
     .post(
         "/" ,
         clerkMiddleware(),
