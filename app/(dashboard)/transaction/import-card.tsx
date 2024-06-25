bb203bb532a751a021edd8b87080c13972283d85
Nikhil chopra <nikhil0080012005@gmail.com>
2024-06-25 19:06:08 +0530

    csv import completed

--- app/(dashboard)/transaction/import-card.tsx
+++ app/(dashboard)/transaction/import-card.tsx
@@ -3,10 +3,12 @@ import {
     CardContent,
     CardHeader,
     CardTitle
- } from "@/components/ui/card";
- import { Button } from "@/components/ui/button";
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 import { useState } from "react";
 import { ImportTable } from "./import-table";
+import { convertAmountToMilliUnits } from "@/lib/utils";
+import { format, parse, isValid } from "date-fns";
 
 const DateFormat = "yyyy-MM-dd HH:mm:ss";
 const outputFormat = "yyyy-MM-dd";
@@ -15,42 +17,42 @@ const requiredOptions = [
     "amount",
     "date",
     "payee",
-]
+];
 
 interface SelectedColumnsState {
-    [key : string]: string | null;
-};
+    [key: string]: string | null;
+}
 
 type Props = {
-    data : string[][],
-    onCancel : () => void;
-    onSubmit : (data : any) => void
+    data: string[][],
+    onCancel: () => void;
+    onSubmit: (data: any) => void;
 }
 
 export const ImportCard = ({
     data,
     onCancel,
     onSubmit,
-} : Props) => {
-    const [ SelectedColumns , setSelectedColumns ] = useState<SelectedColumnsState>({});
+}: Props) => {
+    const [SelectedColumns, setSelectedColumns] = useState<SelectedColumnsState>({});
 
     const Header = data[0];
     const Body = data.slice(1);
 
     const onTableHeadSelectChange = (
-        columnIndex : number,
-        value : string | null
+        columnIndex: number,
+        value: string | null
     ) => {
         setSelectedColumns((prev) => {
-            const newSelectedColumns = {...prev};
+            const newSelectedColumns = { ...prev };
 
-            for(const key in newSelectedColumns){
-                if(newSelectedColumns[key] === value){
+            for (const key in newSelectedColumns) {
+                if (newSelectedColumns[key] === value) {
                     newSelectedColumns[key] = null;
                 }
             }
 
-            if(value === "skip"){
+            if (value === "skip") {
                 value = null;
             }
 
@@ -61,6 +63,64 @@ export const ImportCard = ({
 
     const progress = Object.values(SelectedColumns).filter(Boolean).length;
 
+    const handleContinue = () => {
+        const getColumnIndex = (column: string) => {
+            return column.split("_")[1];
+        };
+
+        const mappedData = {
+            headers: Header.map((_header, index) => {
+                return SelectedColumns[`column_${index}`] || null;
+            }),
+            body: Body.map((row) => {
+                const transformedRow = row.map((cell, index) => {
+                    return SelectedColumns[`column_${index}`] ? cell : null;
+                });
+
+                return transformedRow.every((item) => item === null)
+                    ? []
+                    : transformedRow;
+            }).filter((row) => row.length > 0),
+        };
+
+        // console.log({ mappedData });
+
+        const arrayOfData = mappedData.body.map((row) => {
+            return row.reduce((acc: any, cell, index) => {
+                const header = mappedData.headers[index];
+                if (header != null) {
+                    acc[header] = cell;
+                }
+
+                return acc;
+            }, {});
+        });
+
+        const formattedData = arrayOfData.map((item) => {
+            let formattedDate = item.date?.trim(); // Trim any extra spaces
+            const parsedDate = parse(formattedDate, DateFormat, new Date());
+
+            // console.log(formattedDate)
+            // console.log(parsedDate)
+
+            if (isValid(parsedDate)) {
+                formattedDate = format(parsedDate, outputFormat);
+            } else {
+                console.warn(`Invalid date format: ${item.date}`);
+                formattedDate = "Invalid date"; // or handle invalid date appropriately
+            }
+
+            return {
+                ...item,
+                amount: convertAmountToMilliUnits(parseFloat(item.amount)),
+                date: formattedDate,
+            };
+        });
+
+        // console.log({ formattedData });
+        onSubmit(formattedData);
+    }
+
     return (
         <div className="max-width-2xl mx-auto w-full pb-10 -mt-24">
             <Card className="border-none drop-shadow-sm">
@@ -69,20 +129,20 @@ export const ImportCard = ({
                         Import Transaction 
                     </CardTitle>
                     <div className="flex flex-col lg:flex-row gap-y-2 items-center gap-x-2">
-                    <Button size="sm" 
-                        onClick={onCancel}
-                        className="w-full lg:w-auto"
-                    >
-                        Cancel
-                    </Button>
-                    <Button
-                        size="sm"
-                        disabled={progress < requiredOptions.length}
-                        onClick={() => {}}
-                        className="w-full lg:w-auto"
-                    >
-                        Continue ({progress} / {requiredOptions.length})
-                    </Button>
+                        <Button size="sm" 
+                            onClick={onCancel}
+                            className="w-full lg:w-auto"
+                        >
+                            Cancel
+                        </Button>
+                        <Button
+                            size="sm"
+                            disabled={progress < requiredOptions.length}
+                            onClick={handleContinue}
+                            className="w-full lg:w-auto"
+                        >
+                            Continue ({progress} / {requiredOptions.length})
+                        </Button>
                     </div>
                 </CardHeader>
                 <CardContent>
@@ -93,7 +153,7 @@ export const ImportCard = ({
                         onTableHeadSelectChange={onTableHeadSelectChange}
                     />
                 </CardContent>
-                </Card>
-                </div>
+            </Card>
+        </div>
     )
 }
